f9bb1aca611456fe8d527452fb07fef501325a1c
Alex Vong <avong003@gmail.com>
2022-12-20 12:29:32 -0800

    added quiz questions on quiz data php

---
+++ quiz-data.php
@@ -0,0 +1,90 @@
+
+
+<?php 
+
+	$quiz = [
+		[
+			'questionNumber' => 'q1',
+			'question' => "CSS Stands for",
+			'A' => 'Computer Style Sheets',
+			'B' => 'Creative Style Sheets', 
+			'C' => 'Cascading Style Sheets', 
+			'D' => 'Canvas Styling System', 
+		],
+		[
+			'questionNumber' => 'q2',
+			'question' => "How can you add space between the border and inner content of the element?",
+			'A' => 'margin',
+			'B' => 'padding', 
+			'C' => 'border', 
+			'D' => 'spacing', 
+		],
+		[
+			'questionNumber' => 'q3',
+			'question' => "In the following code snippet, what value have we used for the bottom margin? {margin: 10px 8px 15px 9px;}",
+			'A' => '10px',
+			'B' => '8px', 
+			'C' => '15px', 
+			'D' => '9px', 
+		],
+		[
+			'questionNumber' => 'q4',
+			'question' => "How can you add a comment in a CSS file?",
+			'A' => '// this is a comment',
+			'B' => '/* this is a comment */', 
+			'C' => '// this is a comment //', 
+			'D' => '<-- this is a comment -->', 
+		],
+		[
+			'questionNumber' => 'q5',
+			'question' => "Which selector is used to specify a style for one unique element?",
+			'A' => 'text',
+			'B' => 'class', 
+			'C' => 'style', 
+			'D' => 'id', 
+		],
+		[
+			'questionNumber' => 'q6',
+			'question' => "Which of the following would be used to create an ID called header which has a width of 750px, a height of 30px and the color of the text is black?",
+			'A' => '#header { height: 30px; width: 750px; color: black; }',
+			'B' => '.header { height: 30px; width: 750px; colour: black; }', 
+			'C' => '#header { height: 30px; width: 750px; text: black; }', 
+			'D' => '.header { height: 30px; width: 750px; color: black; }', 
+		],
+
+		[
+			'questionNumber' => 'q7',
+			'question' => "Select the appropriate HTML tag used for the largest heading?",
+			'A' => 'H2',
+			'B' => 'Title', 
+			'C' => 'H1', 
+			'D' => 'Heading', 
+		],
+		[
+			'questionNumber' => 'q8',
+			'question' => "Which of the below CSS properties is used to change the background color of CSS ?",
+			'A' => 'background-color',
+			'B' => 'color-background', 
+			'C' => 'bg-color', 
+			'D' => 'color-bg', 
+		],
+		[
+			'questionNumber' => 'q9',
+			'question' => "Which of the below CSS class is used to change the text color of CSS ?",
+			'A' => 'word-color',
+			'B' => 'font-color', 
+			'C' => 'text-color', 
+			'D' => 'color', 
+		],
+		[
+			'questionNumber' => 'q10',
+			'question' => "How can you change the bottom margin of an element?",
+			'A' => 'margin-bt',
+			'B' => 'margin-bottom', 
+			'C' => 'bottom-margin', 
+			'D' => 'm-b', 
+		]
+	];
+
+
+ ?>
