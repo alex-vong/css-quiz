34f441731e33c98d9bd3edfceb0bbe0865f4e352
Alex Vong <avong003@gmail.com>
2022-12-21 09:58:50 -0800

    updated meta image

--- head.php
+++ head.php
@@ -11,9 +11,9 @@
 		<meta property="og:type" content="">
 		<meta property="og:url" content="">
 
-		<meta property="og:image" content="https://alexvong.dev/images/bros.png"  />
-		<meta property="og:image:type" content="https://alexvong.dev/images/bros.png" />
-		<link rel="shortcut icon" type="image/png" href=[[]] />
+		<meta property="og:image" content="images/meta-img.svg"  />
+		<meta property="og:image:type" content="images/meta-img.svg" />
+		<link rel="shortcut icon" type="image/jpg" href="images/favicon.svg" />
 
 		<link rel='stylesheet' href='css/style.css'>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
